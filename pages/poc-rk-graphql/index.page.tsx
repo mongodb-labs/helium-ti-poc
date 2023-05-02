0d1b14ef76d812baef0f9729937e4f761d9ea382
Rajan Khullar <rajan.khullar@mongodb.com>
2023-05-02 10:25:45 -0400

    save

---
+++ pages/poc-rk-graphql/index.page.tsx
@@ -0,0 +1,27 @@
+import { gql, useQuery } from '@apollo/client';
+
+export function Page() {
+
+    const query = gql`
+    query CurrentUserQuery {
+        CurrentUser {
+            firstName
+            lastName
+            email
+        }
+    }`;
+
+    const {data} = useQuery(query);
+
+    async function test() {
+        console.log('hello world');
+        console.log(data);
+    }
+
+    return (
+        <div>
+            <p>Hello World</p>
+            <button onClick={() => test()}>test</button>
+        </div>
+    );
+}
